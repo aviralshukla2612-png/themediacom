b56986d65df06326a738a80ea423944d50d699ca
Yamini <aviralhukla2612@gmail.com>
2026-08-11 16:05:25 +0530

    Auto-clear global_seo_settings cache when Contact or SEO settings are updated

--- app/Models/SeoSetting.php
+++ app/Models/SeoSetting.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class SeoSetting extends Model
 {
     protected $guarded = [];
+
+    protected static function booted()
+    {
+        static::saved(function ($model) {
+            \Illuminate\Support\Facades\Cache::forget('global_seo_settings');
+        });
+        static::deleted(function ($model) {
+            \Illuminate\Support\Facades\Cache::forget('global_seo_settings');
+        });
+    }
 }
